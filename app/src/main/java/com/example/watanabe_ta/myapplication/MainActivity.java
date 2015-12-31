f1e71c4177adfa1955fca31d95850e33f8b64baa
na8esin <na8esin.mpst@gmail.com>
2015-12-31 15:17:36 +0900

    テスト

---
+++ app/src/main/java/com/example/watanabe_ta/myapplication/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.watanabe_ta.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Click(R.id.button)
+    public void button(View view) {
+
+        EditText editTex2 = (EditText) findViewById(R.id.editText2);
+
+        // エディットテキストのテキストを取得します
+        String text2 = editTex2.getText().toString();
+
+        // 第3引数は、表示期間（LENGTH_SHORT、または、LENGTH_LONG）
+        Toast.makeText(this, text2, Toast.LENGTH_SHORT).show();
+
+        MyAsync ma = new MyAsync(this);
+        ma.execute("hoge");
+    }
+
+}
